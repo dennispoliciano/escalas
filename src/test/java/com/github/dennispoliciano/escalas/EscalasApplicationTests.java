db548eab1376f48fd4a705e5100a316e10d25854
Dennis Policiano <denniscsp.dev@gmail.com>
2026-08-20 17:46:57 -0300

    feat: :art: initial commit

---
+++ src/test/java/com/github/dennispoliciano/escalas/EscalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dennispoliciano.escalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
