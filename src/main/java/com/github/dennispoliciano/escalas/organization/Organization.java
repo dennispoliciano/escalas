d44f057d3183f24305ca0861a5fd252cd66369ef
dennispoliciano <denniscsp.dev@gmail.com>
2026-08-26 13:15:12 -0300

    feat(ESC-003): adiciona nome explicito da tabela organizations

--- src/main/java/com/github/dennispoliciano/escalas/organization/Organization.java
+++ src/main/java/com/github/dennispoliciano/escalas/organization/Organization.java
@@ -1,11 +1,9 @@
 package com.github.dennispoliciano.escalas.organization;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "organizations")
 public class Organization {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
