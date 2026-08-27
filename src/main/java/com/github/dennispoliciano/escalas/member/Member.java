7735cd46eb152aacfc350ec0ec87e9180f0c9cc0
dennispoliciano <denniscsp.dev@gmail.com>
2026-08-27 16:29:33 -0300

    feat(ESC-006): adiciona getter e setter para score

--- src/main/java/com/github/dennispoliciano/escalas/member/Member.java
+++ src/main/java/com/github/dennispoliciano/escalas/member/Member.java
@@ -56,6 +56,14 @@ public class Member {
         this.id = id;
     }
 
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
     public String getName() {
         return name;
     }
