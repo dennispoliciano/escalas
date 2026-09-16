ec223825472669d0ab715db294cd38771fa3084d
dennispoliciano <denniscsp.dev@gmail.com>
2026-09-16 18:04:37 -0300

    feat(esc-012): fix nome da tabela org_memberships

--- src/main/java/com/github/dennispoliciano/escalas/orgmembership/OrgMembership.java
+++ src/main/java/com/github/dennispoliciano/escalas/orgmembership/OrgMembership.java
@@ -14,7 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "org_membership")
+@Table(name = "org_memberships")
 public class OrgMembership {
 
     @Id
